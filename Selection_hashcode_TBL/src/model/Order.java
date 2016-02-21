492000aebbbba44e0daf64b03e4fa9b181db43fb
tbarthel <barthelierry@gmail.com>
2016-02-21 12:42:43 +0100

    Selection_hashcode_TBL/

---
+++ Selection_hashcode_TBL/src/model/Order.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+
+	public int id;
+	public int row;
+	public int col;
+	
+	public Integer deliveredTurn;
+	
+	public int[] products;
+	
+	public Order(int id, int row, int col) {
+		this.id = id;
+		this.row = row;
+		this.col = col;
+		
+		this.deliveredTurn = null;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String toString() {
+		return "{id : " + this.id + ", row : " + this.row + ", col : " + this.col + ", products nb : " + this.products.length + "}";
+	}
+
+}
